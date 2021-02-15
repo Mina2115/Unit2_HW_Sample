4f1d43de2ef0f9a0851507c61363e8e725cb457e
Mina2115 <78247742+Mina2115@users.noreply.github.com>
2021-02-15 11:32:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
+require([
+        "esri/Map",
+        "esri/views/MapView",
+        "esri/layers/Layer"
+      ], function (Map, MapView, Layer) {
         var map = new Map({
-          basemap: "dark-gray"
+          basemap: "topo-vector"
         });
 
         var view = new MapView({
-          container: "viewDiv",
           map: map,
+          container: "viewDiv",
           zoom: 10,
-          center: [-89.85, 38.85] // longitude, latitude
+          center: [-90, 38]
         });
+
+        var layer = Layer.fromPortalItem({
+          portalItem: {
+            // autocasts as new PortalItem()
+            
+        
+          id: "013ed73e422c46d5b45d11dcde931007"
+        }
+        })
+          .then(addLayer)
+          .catch(rejection);
+
+        // Adds the layer to the map once it loads
+        function addLayer(layer) {
+          map.add(layer);
+        }
+
+        function rejection(error) {
+          console.log("Layer failed to load: ", error);
+        }
       });
